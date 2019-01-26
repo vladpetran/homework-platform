ea4cb46d4295e58f12c49963e7761014a47e0021
Vlad Petran <petran_vlad@yahoo.com>
2019-01-26 22:03:35 +0200

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,119 @@
+﻿using BusinessLayer.Models;
+using BusinessLayer.ModelServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EducationalPortal.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        [RoleAuthorize("Administrator")]
+        public ActionResult Teachers()
+        {
+            List<TeacherViewModel> teacherViewList = new List<TeacherViewModel>();
+            TeacherService te = new TeacherService();
+            teacherViewList = te.GetTeachers();
+            return View(teacherViewList);
+        }
+        [RoleAuthorize("Administrator")]
+        public ActionResult CreateTeacher()
+        {
+            return View();
+        }
+        [RoleAuthorize("Administrator")]
+        [HttpPost]//by admin
+        public ActionResult CreateTeacher(TeacherViewModel teacher)
+        {
+            TeacherService regViewModel = new TeacherService();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    regViewModel.AddTeacher(teacher);
+                    return RedirectToAction("Index", "Home", teacher);
+                }
+            }
+            catch (Exception)
+            {
+
+                return View("ErrorRegister");
+            }
+            return View();
+        }
+        [RoleAuthorize("Administrator")]
+        [HttpGet]
+        public ActionResult ChangeTeacher()
+        {
+            TeacherService ts = new TeacherService();
+            List<TeacherViewModel> listTeacher = new List<TeacherViewModel>();
+            listTeacher = ts.GetTeachers();
+            ViewBag.Teachers = listTeacher;
+            List<StudInfoViewModel> studentsList = new List<StudInfoViewModel>();
+            StudentService studentService = new StudentService();
+            studentsList = studentService.GetAllStudents();
+
+            return View(studentsList);
+        }
+        [RoleAuthorize("Administrator")]
+        [HttpPost]
+        public ActionResult UpdateTeacher(int teacherID, int studentID)
+        {
+            StudentService stService = new StudentService();
+            stService.UpdateStudentTeacher(studentID, teacherID);
+            return RedirectToAction("ChangeTeacher");
+        }
+        [RoleAuthorize("Administrator")]
+        public ActionResult GetStudent(int studentId)
+        {
+            TeacherService ts = new TeacherService();
+            List<TeacherViewModel> listTeacher = new List<TeacherViewModel>();
+            listTeacher = ts.GetTeachers();
+            ViewBag.Teachers = listTeacher;
+            StudInfoViewModel studentInfo = new StudInfoViewModel();
+            StudentService studentService = new StudentService();
+            studentInfo = studentService.GetStudByID(studentId);
+
+            return View(studentInfo);
+        }
+
+        [RoleAuthorize("Administrator")]
+        public ActionResult MyHomeworks(int id)
+        {
+            List<HomeworkViewModel> teacherViewList = new List<HomeworkViewModel>();
+            HomeworkService te = new HomeworkService();
+            teacherViewList = te.GetHomeworksByTeacherID(id);
+            return View(teacherViewList);
+
+        }
+        [RoleAuthorize("Administrator")]
+        public ActionResult MyStudents(int id)
+        {
+            List<StudInfoViewModel> studentViewList = new List<StudInfoViewModel>();
+            StudentService student = new StudentService();
+            studentViewList = student.GetStudentsByTeacherID(id);
+            return View(studentViewList);
+        }
+        [HttpGet]
+        [RoleAuthorize("Administrator")]
+        public ActionResult ResetPass()
+        {
+            return View();
+        }
+        [HttpPost]
+        [RoleAuthorize("Administrator")]
+        public ActionResult ResetPass(string email)
+        {
+
+            UserViewModel User = new UserViewModel();
+            bool ChangePass = User.ResetPass(email);
+            ViewBag.Reset = ChangePass;
+            return View("ResetPass");
+        }
+
+
+    }
+}
---
+++ Controllers/StudentHomeworksController.cs
@@ -0,0 +1,112 @@
+﻿using BusinessLayer.Models;
+using BusinessLayer.ModelServices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EducationalPortal.Controllers
+{
+    public class StudentHomeworksController : Controller
+    {
+        [HttpPost]
+        [RoleAuthorize("Student")]
+        public ActionResult UploadHomeworkFiles(StudentHomeworkViewModel sh)
+        {
+            if (sh.files.ElementAt(0) == null)
+            {
+                return RedirectToAction("Homeworks", "Student");
+            }
+            string username = Request.Cookies["UserSettings"].Values["UserName"];
+
+            string dir = "~/Homeworks/" + username + "/" + sh.SHomeID;
+
+            bool exists = System.IO.Directory.Exists(Server.MapPath(dir.ToString()));
+            string myPath = sh.Path;
+
+            foreach (var file in sh.files)
+            {
+                if (file != null)
+                {
+                    if (!exists)
+                        System.IO.Directory.CreateDirectory(Server.MapPath(dir.ToString()));
+
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath(dir.ToString()), fileName);
+                    file.SaveAs(path);
+                    myPath = fileName + " " + myPath;
+                }
+            }
+            StudentHomeworkServices HService = new StudentHomeworkServices();
+            HService.UpdateStudentHomework(sh.SHomeID, 0, "Uploaded", myPath);
+
+            return RedirectToAction("Homeworks", "Student");
+        }
+        [RoleAuthorize("Teacher")]
+        public ActionResult ViewDetails(int studentId, int sHId)
+        {
+            StudentHomeworkServices service = new StudentHomeworkServices();
+            StudentHomeworkViewModel homework = service.GetHomeworkDetails(studentId, sHId);
+
+            return View(homework);
+        }
+        [RoleAuthorize("Teacher")]
+        public ActionResult GetFiles(int studentId, int sHId, string studentName)
+        {
+
+            string path = "~/Homeworks/" + studentName + "/" + sHId + "/";
+            var dir = new System.IO.DirectoryInfo(Server.MapPath(path));
+            System.IO.FileInfo[] fileNames = dir.GetFiles("*.*");
+            List<string> items = new List<string>();
+
+            foreach (var file in fileNames)
+            {
+                items.Add(file.Name);
+            }
+            ViewBag.HomeID = sHId;
+            ViewBag.StudentName = studentName;
+            return View(items);
+        }
+        [RoleAuthorize("Teacher")]
+        public ActionResult OpenFile(int homeID, string FileName, string StudentName)
+        {
+
+            string content = string.Empty;
+            string path = "~/Homeworks/" + StudentName + "/" + homeID + "/" + FileName;
+            using (StreamReader reader = new StreamReader(Server.MapPath(path)))
+            {
+                content = reader.ReadToEnd();
+            }
+
+            ViewBag.Content = content;
+
+            var text = Request.Form["TextAreaName"];
+
+
+
+            return View("GetFiles2");
+        }
+
+        [RoleAuthorize("Teacher")]
+        public ActionResult GetPdf(int homeID, string fileName, string StudentName)
+        {
+            string path = "~/Homeworks/" + StudentName + "/" + homeID + "/" + fileName;
+            var fileStream = new FileStream(Server.MapPath(path),
+                                             FileMode.Open,
+                                             FileAccess.ReadWrite
+                                           );
+            var fsResult = new FileStreamResult(fileStream, "text/plain");
+
+            return fsResult;
+        }
+        [RoleAuthorize("Teacher")]
+        public FileResult Download(int homeID, string FileName, string StudentName)
+        {
+            string path = "~/Homeworks/" + StudentName + "/" + homeID + "/";
+            return File(path + FileName, System.Net.Mime.MediaTypeNames.Application.Octet);
+        }
+
+    }
+}
